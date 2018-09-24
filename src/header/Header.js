203cf6fa6924ac2b513989cc45136137c2a58c4a
unknown <singh_suryaprakash@yahoo.com>
2018-09-24 03:40:00 +0300

    Flight Search Engine

---
+++ src/header/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './header.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="header display--flex">
+       
+        <h2 className="headertitle">Flight Search Engine</h2>
+      </header>
+    );
+  }
+}
+
+export default Header;
---
+++ src/store/flights-json.js
@@ -0,0 +1,95 @@
+const FLIGHTS = [{
+      "id": 1,
+      "airline_code": "ai",
+      "airline": "Air India",
+      "number": "AI-202",
+      "from": "PUNE",
+      "to": "DELHI",
+      "from_code": "PNQ",
+      "to_code": "DEL",
+      "depart_date": "Sep 24 2018 17:03:07 GMT+0530 (IST)",
+      "arrive_date": "Sep 24 2018 19:03:07 GMT+0530 (IST)",
+      "price": 2300,
+      "return_trip": {
+        "airline": "Air India",
+        "number": "AI-203",
+        "from": "DELHI",
+        "to": "PUNE",
+        "from_code": "DEL",
+        "to_code": "PNQ",
+        "depart_date": "Oct 19 2018 10:03:07 GMT+0530 (IST)",
+        "arrive_date": "Oct 19 2018 12:03:07 GMT+0530 (IST)",
+        "price": 200  
+      }
+    }, {
+      "id": 2,
+      "airline_code": "indigo",
+      "airline": "Indigo",
+      "number": "IND-202",
+      "from": "HYDERABAD",
+      "to": "DELHI",
+      "from_code": "HYD",
+      "to_code": "DEL",
+      "depart_date": "Oct 20 2018 15:03:07 GMT+0530 (IST)",
+      "arrive_date": "Oct 20 2018 17:03:07 GMT+0530 (IST)",
+      "price": 2300,
+      "return_trip": {
+        "airline": "Indigo",
+        "number": "IND-203",
+        "from": "DELHI",
+        "to": "HYDERABAD",
+        "from_code": "DEL",
+        "to_code": "HYD",
+        "depart_date": "Oct 21 2018 09:03:07 GMT+0530 (IST)",
+        "arrive_date": "Oct 21 2018 11:03:07 GMT+0530 (IST)",
+        "price": 300        
+      }      
+    }, {
+      "id": 3,
+      "airline_code": "ai",
+      "airline": "Air India",
+      "number": "AI-204",
+      "from": "PUNE",
+      "to": "DELHI",
+      "from_code": "PNQ",
+      "to_code": "DEL",
+      "depart_date": "Oct 22 2018 17:03:07 GMT+0530 (IST)",
+      "arrive_date": "Oct 22 2018 19:03:07 GMT+0530 (IST)",
+      "price": 2300,
+      "return_trip": {
+        "airline": "Air India",
+        "number": "AI-205",
+        "from": "DELHI",
+        "to": "PUNE",
+        "from_code": "DEL",
+        "to_code": "PNQ",
+        "depart_date": "Oct 24 2018 10:03:07 GMT+0530 (IST)",
+        "arrive_date": "Oct 24 2018 12:03:07 GMT+0530 (IST)",
+        "price": 200  
+      }
+    }, {
+      "id": 4,
+      "airline_code": "indigo",
+      "airline": "Indigo",
+      "number": "IND-204",
+      "from": "HYDERABAD",
+      "to": "DELHI",
+      "from_code": "HYD",
+      "to_code": "DEL",
+      "depart_date": "Tue Apr 26 2018 15:03:07 GMT+0530 (IST)",
+      "arrive_date": "Tue Apr 26 2018 17:03:07 GMT+0530 (IST)",
+      "price": 6000,
+      "return_trip": {
+        "airline": "Indigo",
+        "number": "IND-205",
+        "from": "DELHI",
+        "to": "HYDERABAD",
+        "from_code": "DEL",
+        "to_code": "HYD",
+        "depart_date": "Tue Apr 27 2018 09:03:07 GMT+0530 (IST)",
+        "arrive_date": "Tue Apr 27 2018 11:03:07 GMT+0530 (IST)",
+        "price": 1500        
+      }      
+    }];
+
+  export default FLIGHTS;
